5a1d2f44e5a94e19bb73c0f91682875cd378e6f7
Brenden <brenden.niedermeyer@gmail.com>
2020-01-10 21:20:45 -0800

    feat(external schematic): add Prettier to ng add
    This change prompts the user to add Prettier to their project when executing the `ng add` command. If the user chooses yes, it installs and calls [@schuchard/prettier](https://github.c
    om/schuchard/prettier-schematic).
    
    Closes #10

--- src/ng-add/index.ts
+++ src/ng-add/index.ts
@@ -28,6 +28,10 @@ export default function(options: Schema): Rule {
 function prepareDependencies(options: Schema): Rule {
   const deps = ['@angular/material'];
 
+  if (options.addPrettier) {
+    deps.push('@schuchard/prettier');
+  }
+
   switch (options.deployment) {
     case 'Azure':
       deps.push('@azure/ng-deploy');
